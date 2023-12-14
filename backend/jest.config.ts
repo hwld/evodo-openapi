bbac930821df947ab8350d8a7c81313d619a75fc
hwld <0701hw@gmail.com>
2023-12-14 13:26:50 +0900

    chore: jestのセットアップ

---
+++ backend/jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  testEnvironment: "miniflare",
+  testMatch: ["**/*.test.ts"],
+  preset: "ts-jest/presets/js-with-ts",
+  // yamlのパッケージがimportを使ってるので、ts-jestのpresetでCJSに変換する
+  // デフォルトだとnode_modulesが全部ignoreに入ってるので、yaml以外を指定する。ここの書き方なんか間違ってるかも
+  // また、tsconfig.jsonでallowJsをtrueにする必要がある。
+  // https://kulshekhar.github.io/ts-jest/docs/getting-started/presets/
+  transformIgnorePatterns: ["node_modules/.pnpm/(?!(yaml))"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
+
+module.exports = config;
