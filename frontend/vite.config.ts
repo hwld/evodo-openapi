366999f5a59122163c1011bc3c10f655e099d4eb
hwld <0701hw@gmail.com>
2023-12-24 11:39:37 +0900

    chore: shadcn-uiを入れた

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
@@ -5,4 +6,5 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 });
