e52f103fb1daea622adcbb13ee64ba0143bb649b
hwld <0701hw@gmail.com>
2023-12-24 18:52:23 +0900

    chore: 色々変更

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "@tanstack/react-router";
 
 export const RootLayout: React.FC = () => {
   return (
-    <div className="h-[100dvh] bg-gray-200 text-gray-700 p-5">
+    <div className="h-[100dvh]">
       <Outlet />
     </div>
   );
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,16 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import Unfonts from "unplugin-fonts/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Unfonts({
+      google: { families: [{ name: "Noto Sans JP", styles: "wght@500;700" }] },
+    }),
+  ],
   server: { port: 5173 },
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 });
