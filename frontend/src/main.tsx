4c3ef3e7584d6816a4714f8912123dd20b45f08d
hwld <0701hw@gmail.com>
2023-12-25 15:33:44 +0900

    feat: zodのエラーメッセージを日本語にする

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,18 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./main.css";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./routes.tsx";
+import i18next from "i18next";
+import { zodI18nMap } from "zod-i18n-map";
+import translation from "zod-i18n-map/locales/ja/zod.json";
+import { z } from "zod";
+
+i18next.init({
+  lng: "ja",
+  resources: {
+    ja: { zod: translation },
+  },
+});
+z.setErrorMap(zodI18nMap);
 
 const queryClient = new QueryClient();
 
