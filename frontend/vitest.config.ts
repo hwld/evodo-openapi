4a6401ac8af0fb5b5339f2cfaf0f56ecd3330283
hwld <0701hw@gmail.com>
2024-01-09 01:24:59 +0900

    chore: mswと相性が悪いのでhappy-domではなくjsdomを使う

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -6,7 +6,7 @@ export default mergeConfig(
   defineConfig({
     test: {
       globals: true,
-      environment: "happy-dom",
+      environment: "jsdom",
       setupFiles: ["./vitest-setup.ts"],
     },
   }),
